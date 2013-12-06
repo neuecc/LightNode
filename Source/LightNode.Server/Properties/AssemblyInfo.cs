f1ff329187027c4b1b3f74a19b28d98b696b74c4
neuecc <ils@neue.cc>
2013-12-07 00:26:53 +0900

    rename to LightNode.Owin -> LightNode.Server

--- Source/LightNode.Server/Properties/AssemblyInfo.cs
+++ Source/LightNode.Server/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("LightNode.Owin")]
+[assembly: AssemblyTitle("LightNode.Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
