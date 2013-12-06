a646aab3ea4f089d1b5b0b9d59a4de2d7b070756
neuecc <ils@neue.cc>
2013-12-07 00:23:57 +0900

    initial commit

---
+++ Sample/LightNode.Sample.Server.SelfHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightNode.Sample.Server.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
