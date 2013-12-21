942c34dd2eb53c83827b26363e9249144d0b9108
neuecc <ils@neue.cc>
2013-12-21 16:49:43 +0900

    Add OwinMockServer #14

---
+++ Tests/LightNode.Server.Tests/MockEnv.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin;
+using System.Net.Http;
+
+namespace LightNode.Server.Tests
+{
+    // Owin Mock Environment
+
+    [TestClass]
+    public class MockEnv
+    {
+        public static TestServer TestServer { get; private set; }
+
+        public static HttpClient CreateHttpClient()
+        {
+            return new HttpClient(TestServer.Handler);
+        }
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext cx)
+        {
+            TestServer = TestServer.Create(app =>
+            {
+                app.UseLightNode(
+                    new LightNodeOptions(AcceptVerbs.Get | AcceptVerbs.Post, new JavaScriptContentTypeFormatter())
+                    , typeof(MockEnv).Assembly);
+            });
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            if (TestServer != null) TestServer.Dispose();
+        }
+    }
+}
--- Tests/LightNode.Server.Tests/UnitTest1.cs
+++ Tests/LightNode.Server.Tests/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Owin.Testing;
+using Owin;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace LightNode.Server.Tests
 {
@@ -9,6 +13,16 @@ namespace LightNode.Server.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var client = MockEnv.CreateHttpClient();
+            client.GetStringAsync("http://localhost/Moge/Hello").Result.Is("\"Hello\"");
+        }
+    }
+
+    public class Moge : ILightNodeContract
+    {
+        public string Hello()
+        {
+            return "Hello";
         }
     }
 }
