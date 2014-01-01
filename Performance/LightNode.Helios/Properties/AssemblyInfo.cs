2d67ae3292f57365372b22bfce832d459b79160a
neuecc <ils@neue.cc>
2014-01-02 00:07:43 +0900

    add Helios PerfTest

---
+++ Performance/LightNode.Helios/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は、以下の属性セットによって 
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("LightNode.Helios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LightNode.Helios")]
+[assembly: AssemblyCopyright("Copyright (C)  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、 
+// COM コンポーネントがこのアセンブリ内のその型を認識できなくなります。
+// COM からこのアセンブリ内の型にアクセスする必要がある場合は、その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("54768dc3-0d05-49ae-a7dd-3ce633aa2952")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン 
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、下のように "*" を使ってリビジョンおよびビルド番号を 
+// 既定値にすることができます:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Performance/LightNode.Helios/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LightNode.Helios
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseLightNode(new LightNode.Server.LightNodeOptions(Server.AcceptVerbs.Get | Server.AcceptVerbs.Post,
+                new LightNode.Formatter.JsonNetContentFormatter()));
+        }
+    }
+
+    public class Perf : LightNode.Server.LightNodeContract
+    {
+        public MyClass Echo(string name, int x, int y, MyEnum e)
+        {
+            return new MyClass { Name = name, Sum = (x + y) * (int)e };
+        }
+    }
+
+    public class MyClass
+    {
+        public string Name { get; set; }
+        public int Sum { get; set; }
+    }
+
+    public enum MyEnum
+    {
+        A = 2,
+        B = 3,
+        C = 4
+    }
+}
