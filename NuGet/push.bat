e7ac1bc342ed5972c0fea207d145393c2f1d3a39
neuecc <ils@neue.cc>
2014-01-14 04:33:51 +0900

    Prepare 0.2.0 nupkg

--- NuGet/push.bat
+++ NuGet/push.bat
@@ -1,6 +1,6 @@
-nuget push LightNode.Core.0.1.1.nupkg
-nuget push LightNode.Server.0.1.1.nupkg
-nuget push LightNode.Client.PCL.T4.0.1.1.nupkg
-nuget push LightNode.Formatter.ProtoBuf.0.1.1.nupkg
-nuget push LightNode.Formatter.MsgPack.0.1.1.nupkg
-nuget push LightNode.Formatter.JsonNet.0.1.1.nupkg
+nuget push LightNode.Core.0.2.0.nupkg
+nuget push LightNode.Server.0.2.0.nupkg
+nuget push LightNode.Client.PCL.T4.0.2.0.nupkg
+nuget push LightNode.Formatter.ProtoBuf.0.2.0.nupkg
+nuget push LightNode.Formatter.MsgPack.0.2.0.nupkg
+nuget push LightNode.Formatter.JsonNet.0.2.0.nupkg
