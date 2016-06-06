9dfef4b6c591149f6bfd2570e38dcb87555dd1f0
neuecc <ils@neue.cc>
2016-06-06 11:28:31 +0900

    LightNode.Formatter.Jil 1.6.3.1

--- NuGet/push.bat
+++ NuGet/push.bat
@@ -5,7 +5,7 @@ nuget push LightNode.Client.UniRx.T4.1.6.3.nupkg
 nuget push LightNode.Formatter.ProtoBuf.1.6.3.nupkg
 nuget push LightNode.Formatter.MsgPack.1.6.3.nupkg
 nuget push LightNode.Formatter.JsonNet.1.6.3.nupkg
-nuget push LightNode.Formatter.Jil.1.6.3.nupkg
+nuget push LightNode.Formatter.Jil.1.6.3.1.nupkg
 nuget push LightNode.Formatter.Jil.LZ4.1.6.3.nupkg
 nuget push Glimpse.LightNode.1.6.3.nupkg
 nuget push LightNode.Swagger.1.6.3.nupkg
